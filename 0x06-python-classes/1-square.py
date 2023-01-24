bed3df9dedb44a9ee7574e8a6a5b11f8bdcd5107
Daniel <funniyou@gmail.com>
2023-01-24 13:14:56 -0800

    0x06-py-classes task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Module of a Square class
+'''
+
+class Square:
+    '''A simple class that defines a square
+    Attributes:
+    size: The size of the square
+    '''
+
+    def __init__(self, size):
+    '''Initialization method for the Square class
+    Args:
+    size: The size of the square
+    '''
+    self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+Module of a Square class
+'''
+
+class Square:
+    '''A simple class that defines a square
+    Attributes:
+    size: The size of the square
+    '''
+    def __init__(self, size=0):
+    '''Initialization method for the Square class
+    Args:
+    size(int): The size of the square
+    '''
+    if isinstance(size, int):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    else:
+        raise TypeError("size must be an integer")
+    def area(self):
+    '''Finds the area of the Square
+    Returns:
+    The area of the square
+    '''
+    return self.__size ** 2
