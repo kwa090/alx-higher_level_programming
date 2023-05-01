76abf2d555860872faefba9d73edec5dcd041e3c
Daniel <funniyou@gmail.com>
2023-05-01 12:10:05 -0700

    correction3

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """
-Same as Task 0 but using request package
+Write a Python script that fetches https://alx-intranet.hbtn.io/status
 """
-
 import requests
 
-
-if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
+if __name__ == '__main__':
+    response = requests.get(url='https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
